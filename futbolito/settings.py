c3239183163b28d1103035ac03e68f5a948edf3c
JackAndrewG <jackson.guzman@unl.edu.ec>
2019-05-17 09:07:42 -0500

    conexion con mysql - archivo instrcciones.txt

--- futbolito/settings.py
+++ futbolito/settings.py
@@ -75,11 +75,15 @@ WSGI_APPLICATION = 'futbolito.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'mysql_cymysql',
+            'NAME': 'futbolitoDB',
+            'USER': 'JackAndrew',
+            'PASSWORD': 'jack',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
