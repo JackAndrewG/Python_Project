374c280dd59076912a8541034b605c18721e55bd
JackAndrewG <jackson.guzman@unl.edu.ec>
2019-05-22 12:24:38 -0500

    gitignore

--- app1/urls.py
+++ app1/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='principal'),
+    path('complejo', views.post_list, name='view_Complejo'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'app1/principal.html', {})
+
+def Complejo(request):
+    return render(request, 'app1/view_Complejo.html', {})
