074e13f1ac8b6f9ba3725efbc7777688c7682624
albert1299 <albert.mora@unl.edu.ec>
2019-07-06 14:22:25 -0500

    Formulario de Reserva completo !

--- app1/models.py
+++ app1/models.py
@@ -3,46 +3,41 @@ from django.db import models
 
 class Complejo (models.Model):
     usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    nombre_complejo = models.CharField(max_length=40, default='Complejo Deportivo ')
+    nombre_complejo = models.CharField(max_length=40, default='Complejo Deportivo')
     direccion_complejo = models.TextField(max_length=200, default='sector-calle principal-calle secundaria-nro-referencia')
     telefono_complejo = models.CharField(max_length=19, default='(+593) 098-234-1334')
     puntuacion_complejo = models.IntegerField(default='5')
+    foto_complejo = models.CharField(max_length=250, null=True, blank=True)
+    coordenadas_complejo = models.CharField(max_length=250, null=True, blank=True)
+    hora_apertura = models.TimeField(default='09:00')
+    hora_cierre = models.TimeField(default='23:00')
     estado_complejo = models.BooleanField(default = True)
 
     def Publish(self):
-        cadena = "{0}, {1}, {2}"
-        return cadena.format(self.nombre_complejo, self.puntuacion_complejo, self.estado_complejo)
-
+        cadena = "{0}"
+        return cadena.format(self.nombre_complejo)
 
     def __str__(self):
         return self.Publish()
 
 class Cancha (models.Model):
-    codigo_cancha = models.CharField(max_length=3, default='001')
-    descripcion_cancha = models.TextField(max_length=150, default='cancha de futbolito')
-    complejo = models.ForeignKey(Complejo,
-    on_delete=models.CASCADE)
-    # DEBUG: 'auth.User'
-    # WARNING: 'auth.User'
+    complejo = models.ForeignKey(Complejo, on_delete=models.CASCADE)
+    descripcion_cancha = models.CharField(max_length=200, default='Cancha de fútbol Nro 1')
     valor_dia = models.DecimalField(max_digits=7, decimal_places=2)
-    valor_noche = models.DecimalField(max_digits=7, decimal_places=2)
-    fecha_creacion = models.DateTimeField(auto_now = True)
+    valor_noche = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    foto_cancha = models.CharField(max_length=250, null=True, blank=True)
     estado_cancha = models.BooleanField(default = True)
 
     def Publish(self):
-        cadena = "{0}, {1}, {2}"
-        return cadena.format(self.codigo_cancha, self.descripcion_cancha, self.estado_cancha)
-
+        cadena = "{0}"
+        return cadena.format(self.descripcion_cancha)
 
     def __str__(self):
         return self.Publish()
 
-
 class Reserva (models.Model):
-    codigo_reserva = models.CharField(max_length=6, default='000123')
     usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     cancha = models.ForeignKey(Cancha, on_delete=models.CASCADE)
-    #numero_cancha relacionar
     fecha_reserva = models.DateField()
     hora_inicio = models.TimeField(default='12:00')
     hora_fin = models.TimeField(default='13:00')
@@ -52,9 +47,8 @@ class Reserva (models.Model):
     estado_reserva = models.BooleanField(default=True)
 
     def Publish(self):
-        cadena = "{0}, {1}, {2}"
-        return cadena.format(self.usuario, self.fecha_creacion, self.fecha_reserva)
-
+        cadena = "{0}, {1}"
+        return cadena.format(self.usuario, self.fecha_reserva)
 
     def __str__(self):
         return self.Publish()
