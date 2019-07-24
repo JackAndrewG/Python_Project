a22975ea36b2386a1d7ffe9f461b6ca1ec7bde7c
Jimmy Vicente <jimmy.vicente@unl.edu.ec>
2019-07-24 12:02:57 -0500

    arreglo

--- futbolito/urls.py
+++ futbolito/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
 	path('api/', include('api.urls')),
     url(r'^documentacion/', schema_view)
 ]
-]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
