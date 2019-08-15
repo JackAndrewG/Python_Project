e9853b113777909135acd3e1fdfc6d41db20d9df
Jimmy Vicente <jimmy.vicente@unl.edu.ec>
2019-08-15 16:18:29 -0500

    corrrecion api Suscripcion

--- futbolito/settings.py
+++ futbolito/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b*jsfdvm28xj4qp4#b43%cuj$m5jstnmq3h&$c%6co!&#a^!*4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '10.20.57.153'] #<Ip de pc me
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '192.168.0.15'] #<Ip de pc me
 
 
 # Application definition
